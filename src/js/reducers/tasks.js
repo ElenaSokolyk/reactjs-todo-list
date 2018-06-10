9bf62ca2d3b5b5ef4eb192c3133782ee3d0c4562
elenasokolik <sokoliklena@ukr.net>
2018-06-10 20:05:46 +0300

    remove project tasks on project removal

--- src/js/reducers/tasks.js
+++ src/js/reducers/tasks.js
@@ -1,7 +1,11 @@
+import reject from "lodash/reject"
+
 export const tasksReducer = (state = [], action) => {
   switch (action.type) {
     case "ADD_TASK":
       return [...state, action.task]
+    case "DELETE_PROJECT":
+      return reject(state, ["projectId", action.id])
     default:
       return state
   }
